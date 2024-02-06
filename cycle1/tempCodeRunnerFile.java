0f58b91b3a602ed309c35f83361b5f4ffbfc1016
ImSreyas <sreyassatheesh755@gmail.com>
2024-02-06 22:22:35 +0530

    An unwanted file removed 'tempCodeRunner.java'.

--- cycle1/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-ssor processor = cpu.new Processor(8, "Intel");
-        cpu.Processor.Ram ram= processor.new Ram(16, "Samsung"); 
-
-        System.out.println("Cpu price is : " + cpu.price + "\n");
-        System.out.println("processor no.of core is : " + processor.noOfCores);
-        System.out.println("processor manufacturer is : " + processor.manufacturer + "\n");
-        System.out.println("Ram size is : " + ram.memory);
-        System.out.pr
