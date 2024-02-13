52669b32c99c2d7a136850ec91166fef4b6f3612
ImSreyasCollege <sreyassatheesh755@gmail.com>
2024-02-13 16:11:57 +0530

    First two programs added to the cycle 2

---
+++ cycle2/SearchElement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Search{
+    public static void main(String arg[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array elements : ");
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Array is : " + Arrays.toString(arr));
+        System.out.print("Enter the value to search : ");
+        int value = scanner.nextInt();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+            if(value == arr[i]){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0) System.out.println("Element " + value + " not found in the array.");
+        else System.out.println("Element " + value + " found in the array.");
+    }
+}
---
+++ cycle2/StringSorting.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class StringSorting{
+    public static void main(String arg[]){
+        Scanner scan = new Scanner(System.in);
+		System.out.println("Name : Sreyas Satheesh\nRoll.no : 53\nTitle : String sorting\nDate : 13/02/2024\n");
+
+        System.out.print("Enter the no.of Strings : ");
+        int n = scan.nextInt();
+        System.out.println("Enter the strings");
+        String[] str = new String[n];
+        scan.nextLine();
+        for(int i=0; i<n; i++){
+            str[i] = scan.nextLine();
+        }
+        System.out.println("Array before sorting : " + Arrays.toString(str));
+        // sorting method 1
+
+        // Arrays.sort(str);
+        // System.out.println(Arrays.toString(str));
+
+        // sorting method 2
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(str[j].compareTo(str[j+1]) > 0){
+                    String temp = str[j];
+                    str[j] = str[j+1];
+                    str[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Array after sorting : " + Arrays.toString(str));
+    }
+}
