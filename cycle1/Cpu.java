9c078bec238a659222157e65033d2cea92fd0c33
ImSreyas <sreyassatheesh755@gmail.com>
2024-02-25 09:11:53 +0530

    :fire: Code refactor.

--- cycle1/Cpu.java
+++ cycle1/Cpu.java
@@ -1,45 +1,121 @@
-// Sreyas - 53
 import java.util.Scanner;
 
-public class Cpu{
-    public static void main(String[] arg){
-		System.out.println("Name : Sreyas Satheesh\nRoll.no : 53\nTitle : Display CPU specs\nDate : 13/02/2024\n");
-        class CpuClass{
-            int price;
+class Cpu {
+    private int price;
+    
+    public Cpu(int price) {
+        this.price = price;
+    }
+    
+    public void printCPUInfo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Processor information");
+        System.out.print("Number of cores : ");
+        int noOfCores = scanner.nextInt();
+        System.out.print("Manufacturer : ");
+        scanner.nextLine();
+        String ProcessorManufacturer = scanner.nextLine();
+        System.out.println("RAM information");
+        System.out.print("Memory size : ");
+        int size = scanner.nextInt();
+        System.out.print("Manufacturer : ");
+        scanner.nextLine();
+        String manufacturer = scanner.nextLine();
+        scanner.close();
 
-            public CpuClass(int price){
-                this.price = price;
-            }
+        System.out.println("\nCPU Price: " + price);
+        System.out.println("Processor and RAM information:");
 
-            class Processor{
-                int noOfCores;
-                String manufacturer;
-
-                public Processor(int noOfCores, String manufacturer){
-                    this.noOfCores = noOfCores;
-                    this.manufacturer = manufacturer;
-                }
-
-                static class Ram{
-                    int memory;
-                    String manufacturer;
-
-                    public Ram(int memory, String manufacturer){
-                        this.memory = memory;
-                        this.manufacturer = manufacturer;
-                    }
-                }
-            }
+        Processor processor = new Processor(noOfCores, ProcessorManufacturer); 
+        RAM ram = RAM.getRAM(size, manufacturer); 
+        processor.printProcessorInfo();
+        ram.printRAMInfo();
+    }
+    
+    public static class Processor {
+        private int numberOfCores;
+        private String manufacturer;
+        
+        public Processor(int numberOfCores, String manufacturer) {
+            this.numberOfCores = numberOfCores;
+            this.manufacturer = manufacturer;
         }
 
-        CpuClass cpu = new CpuClass(1000);
-        CpuClass.Processor processor = cpu.new Processor(8, "Intel");
-        CpuClass.Processor.Ram ram= new CpuClass.Processor.Ram(16, "Samsung"); 
+        public void printProcessorInfo() {
+            System.out.println("  Number of Cores: " + numberOfCores);
+            System.out.println("  Manufacturer: " + manufacturer);
+        }
+    }
 
-        System.out.println("Cpu price is : " + cpu.price + "\n");
-        System.out.println("processor no.of core is : " + processor.noOfCores);
-        System.out.println("processor manufacturer is : " + processor.manufacturer + "\n");
-        System.out.println("Ram size is : " + ram.memory);
-        System.out.println("Ram manufacturer is : " + ram.manufacturer);
+    public static class RAM {
+        private int memory;
+        private String manufacturer;
+        
+        private RAM(int memory, String manufacturer) {
+            this.memory = memory;
+            this.manufacturer = manufacturer;
+        }
+
+        public static RAM getRAM(int memory, String manufacturer) {
+            return new RAM(memory, manufacturer); // Creates and returns a new RAM object
+        }
+        
+        public void printRAMInfo() {
+            System.out.println("  RAM Memory: " + memory + " GB");
+            System.out.println("  Manufacturer: " + manufacturer);
+        }
+    }
+
+    public static void main(String[] args) {
+		System.out.println("Name : Sreyas Satheesh\nRoll.no : 53\nTitle : Display CPU specs\nDate : 13/02/2024\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the price of the cpu : ");
+        Cpu cpu = new Cpu(scanner.nextInt());
+        cpu.printCPUInfo();
+        scanner.close();
     }
 }
+
+// public class Cpu{
+//     public static void main(String[] arg){
+// 		System.out.println("Name : Sreyas Satheesh\nRoll.no : 53\nTitle : Display CPU specs\nDate : 13/02/2024\n");
+//         class CpuClass{
+//             int price;
+
+//             public CpuClass(int price){
+//                 this.price = price;
+//             }
+
+//             class Processor{
+//                 int noOfCores;
+//                 String manufacturer;
+
+//                 public Processor(int noOfCores, String manufacturer){
+//                     this.noOfCores = noOfCores;
+//                     this.manufacturer = manufacturer;
+//                 }
+
+//                 static class Ram{
+//                     int memory;
+//                     String manufacturer;
+
+//                     public Ram(int memory, String manufacturer){
+//                         this.memory = memory;
+//                         this.manufacturer = manufacturer;
+//                     }
+//                 }
+//             }
+//         }
+
+//         CpuClass cpu = new CpuClass(1000);
+//         CpuClass.Processor processor = cpu.new Processor(8, "Intel");
+//         CpuClass.Processor.Ram ram= new CpuClass.Processor.Ram(16, "Samsung"); 
+
+//         System.out.println("Cpu price is : " + cpu.price + "\n");
+//         System.out.println("processor no.of core is : " + processor.noOfCores);
+//         System.out.println("processor manufacturer is : " + processor.manufacturer + "\n");
+//         System.out.println("Ram size is : " + ram.memory);
+//         System.out.println("Ram manufacturer is : " + ram.manufacturer);
+//     }
+// }
--- cycle1/Product.java
+++ cycle1/Product.java
@@ -10,13 +10,13 @@ class ProductClass{
 		this.price = price;
 	}
 }
-public class Product {
+class Product {
 
 	public static void main(String[] arguments){
-		System.out.println("Name : Sreyas Satheesh\nRoll.no : 53\nTitle : Product Class\nDate : 13/02/2024\n");
+		System.out.println("\nName : Sreyas Satheesh\nRoll.no : 53\nTitle : Product Class\nDate : 13/02/2024\n");
 
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the code of the first product : ");
+		System.out.print("\nEnter the code of the first product : ");
 		int code1 = scan.nextInt();
 		System.out.print("Enter the name of the first product : ");
 		scan.nextLine();
@@ -24,7 +24,7 @@ public class Product {
 		System.out.print("Enter the price of the first product : ");
 		int price1 = scan.nextInt();
 
-		System.out.print("Enter the code of the second product : ");
+		System.out.print("\nEnter the code of the second product : ");
 		int code2 = scan.nextInt();
 		System.out.print("Enter the name of the second product : ");
 		scan.nextLine();
@@ -32,21 +32,21 @@ public class Product {
 		System.out.print("Enter the price of the second product : ");
 		int price2 = scan.nextInt();
 
-		System.out.print("Enter the code of the first product : ");
+		System.out.print("\nEnter the code of the third product : ");
 		int code3 = scan.nextInt();
-		System.out.print("Enter the name of the first product : ");
+		System.out.print("Enter the name of the third product : ");
 		scan.nextLine();
 		String name3 = scan.nextLine();
-		System.out.print("Enter the price of the first product : ");
+		System.out.print("Enter the price of the third product : ");
 		int price3 = scan.nextInt();
 
 		ProductClass obj1 = new ProductClass(name1, code1, price1);
 		ProductClass obj2 = new ProductClass(name2, code2, price2);
 		ProductClass obj3 = new ProductClass(name3, code3, price3);
 
-		int minimum = obj1.price < obj2.price ? obj1.price : obj2.price;
-		minimum = minimum < obj3.price ? minimum : obj3.price;
-		System.out.println("Smallest price is : " + minimum);
+		ProductClass minimum = obj1.price < obj2.price ? obj1 : obj2;
+		minimum = minimum.price < obj3.price ? minimum : obj3;
+		System.out.println("\nThe product with the smallest price is " + minimum.pname + " with a price of " + minimum.price + "\n");
 	}
 }
 
