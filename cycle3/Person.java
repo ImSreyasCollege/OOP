9605f57284b78de5480dfa8040de234ca7a70692
ImSreyasCollege <sreyassatheesh755@gmail.com>
2024-04-02 16:31:32 +0530

    3rd program in cycle 3 is added.

---
+++ cycle3/Person.java
@@ -0,0 +1,39 @@
+class Person {
+    String name, gender, address;
+    int age;
+
+    public Person(String name, String gender, String address, int age){
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.age = age;
+    }
+}
+
+class Employee extends Person {
+    int empId, salary;
+    String companyName, qualification;
+
+    public Employee(int empId, String companyName, String qualification, int salary, String name, String gender, String address, int age){
+        super(name, gender, address, age);
+        this.empId = empId;
+        this.companyName = companyName;
+        this.qualification = qualification;
+        this.salary = salary;
+    }
+}
+
+class Teacher extends Employee {
+    int teacherId;
+    String subject, department;
+
+    public Teacher (int teacherId, String subject, String department, int empId, String companyName, String qualification, int salary, String name, String gender, String address, int age){
+        super(empId, companyName, qualification, salary, name, gender, address, age);
+        this.teacherId = teacherId;
+        this. subject = subject;
+        this.department = department;
+    }
+    void display(){
+        
+    }
+}
