3dbe6ced5ca29a7a4fa302f8cf30a9882929e827
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-07 22:08:02 +0530

    More code added to 3rd program in cycle 3.

--- cycle3/Person.java
+++ cycle3/Person.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Person {
     String name, gender, address;
     int age;
@@ -7,6 +9,9 @@ class Person {
         this.gender = gender;
         this.address = address;
         this.age = age;
+    }
+    void display(){
+        System.out.println("Name: " + name + " gender: " + gender + " address: " + address + " age: " + age);
     }
 }
 
@@ -21,6 +26,10 @@ class Employee extends Person {
         this.qualification = qualification;
         this.salary = salary;
     }
+    void display(){
+       System.out.println("Employee Id : " + empId + " Company name : " + companyName + " Qualification : " + qualification + " Salary : " + salary); 
+       super.display();
+    }
 }
 
 class Teacher extends Employee {
@@ -34,6 +43,22 @@ class Teacher extends Employee {
         this.department = department;
     }
     void display(){
-       System.out.println(""); 
+       System.out.println("Teacher Id : " + teacherId + " Department : " + department + " Subject : " + subject); 
+       super.display();
+    }
+}
+
+class PersonMain {
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the no.of Teachers : ");
+        int TeacherCount = scanner.nextInt();
+
+        Teacher [] Teachers = new Teacher[TeacherCount];
+        for (int i=0; i<TeacherCount; i++) {
+            // Teachers[i] = new Teacher();
+        }
+
+        scanner.close();
     }
 }
