027acc73499157bb62b8bf93a1aeb559d528dc7a
ImSreyas <sreyassatheesh755@gmail.com>
2024-07-10 15:36:02 +0530

    exam

--- exam/Test.java
+++ exam/Test.java
@@ -1,71 +1,3 @@
-<h1 align="center">OOP</h1>
-<div align="center">Repo for Object Oriented Programming lab (sem-2)</div>
-
-##
-
-<details>
-<summary>
-<h3> 1) Normal nested classes </h3> 
-</summary>
-
-```java
-class OuterClass {
-    class InnerClass {
-        void innerDisplay() {
-            System.out.println("Message from inner class");
-        }
-    }
-    
-    void display() {
-        System.out.println("Message from outer class");
-    }
-}
-public class nestedClass {
-    public static void main(String[] args) {
-        OuterClass outer = new OuterClass();
-        outer.display();
-        OuterClass.InnerClass inner = outer.new InnerClass();
-        inner.innerDisplay();
-    }
-}
-```
-##
-</details>
-<details>
-<summary>
-<h3> 2) Static nested classes </h3> 
-</summary>
-
-```java
-class OuterClass {
-    static class InnerClass {
-        void innerDisplay() {
-            System.out.println("Message from inner class");
-        }
-    }
-    
-    void display() {
-        System.out.println("Message from outer class");
-    }
-}
-public class nestedClass {
-    public static void main(String[] args) {
-        OuterClass outer = new OuterClass();
-        outer.display();
-        OuterClass.InnerClass inner = new OuterClass.InnerClass();
-        inner.innerDisplay();
-    }
-}
-```
-##
-</details>
-
-<details>
-<summary>
-<h3> 3) cpu nested static and normal classes </h3> 
-</summary>
-
-```java
 import java.util.Scanner;
 
 class CPU {
@@ -140,6 +72,3 @@ public class Test {
         scanner.close();
     }
 }
-```
-##
-</details>
